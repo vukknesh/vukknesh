f76870f74a5214f4f1776974e3d8a6aab85c1f93
Leonardo Monte Castro Neshich <46389448+vukknesh@users.noreply.github.com>
2019-01-18 14:07:18 -0200

    Add files via upload

---
+++ build/precache-manifest.19a116fa7bce81c62455ab3235719d52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c3103e8b994b7c551cbd",
+    "url": "/static/js/main.c3103e8b.chunk.js"
+  },
+  {
+    "revision": "91fd3ffd1d089336e4d6",
+    "url": "/static/js/1.91fd3ffd.chunk.js"
+  },
+  {
+    "revision": "c3103e8b994b7c551cbd",
+    "url": "/static/css/main.7be0bf7f.chunk.css"
+  },
+  {
+    "revision": "91fd3ffd1d089336e4d6",
+    "url": "/static/css/1.812bc3dc.chunk.css"
+  },
+  {
+    "revision": "b99b88512ee589aa70941e3217ca3fc7",
+    "url": "/index.html"
+  }
+];
